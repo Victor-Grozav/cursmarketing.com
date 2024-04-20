3e7ccfb81e3f77a97a59655b6308ee3c25e64fd9
Victor-Grozav <153352553+Victor-Grozav@users.noreply.github.com>
2024-04-20 13:20:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let menu = document.querySelector('#menu-icon');
+    let navlist = document.querySelector('.navlist');
+  
+    menu.onclick = () => {
+      menu.classList.toggle('bx-x');
+      navlist.classList.toggle('open');
+    }
+  });
+
